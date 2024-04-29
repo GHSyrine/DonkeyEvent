4816e31eecbc66d51195d9024a428cdc9701fb07
Syrine GHRIBI <ghribi.syrine.ghs@gmail.com>
2024-04-29 17:13:51 +0200

    Creation of Customer.php and Show.php

---
+++ Model/Entity/Customer.php
@@ -0,0 +1,42 @@
+<?php
+include "Entity.php";
+Class Customer extends Entity {
+    private string $firstname;
+    private string $lastname; 
+    private string $email; 
+    
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+ 
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
---
+++ Model/Entity/Show.php
@@ -0,0 +1,72 @@
+<?php
+include "Entity.php";
+class Show extends Entity{
+    private string $date;
+    private int $price; 
+    private string $langage;
+    private int $movie_id; 
+    private int $room_id; 
+    
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+        return $this; 
+    }
+  
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    
+    public function getLangage()
+    {
+        return $this->langage;
+    }
+
+   
+    public function setLangage($langage)
+    {
+        $this->langage = $langage;
+
+        return $this;
+    }
+
+    
+    public function getMovie_id()
+    {
+        return $this->movie_id;
+    }
+
+    public function setMovie_id($movie_id)
+    {
+        $this->movie_id = $movie_id;
+
+        return $this;
+    }
+
+
+    public function getRoom_id()
+    {
+        return $this->room_id;
+    }
+
+    public function setRoom_id($room_id)
+    {
+        $this->room_id = $room_id;
+
+        return $this;
+    }
+}
