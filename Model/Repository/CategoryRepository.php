c727ffd9eec6ce6d4f0cfef384067aa4afc7b9e6
Alexandra SALVADOR <apintosalvador19@gmail.com>
2024-04-30 18:11:03 +0200

    ajout category repository

---
+++ Model/Repository/CategoryRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "EntityRepository.php";
+
+class CategoryRepository extends EntityRepository
+{
+    public function __construct(PDO $pdo = null)
+    {
+        parent::__construct($pdo, "category");
+    }
+
+    public function getMoviesByCategoryId($id){
+        $tables = ["movie", "movie_category", "category", "movie_category"];
+        $foreignKeys = ['movie.id = movie_category.movie_id', 'category.id = movie_category.category_id'];
+        $movies = $this->getByFiltreJoinTables2($tables, $foreignKeys, "movie.name", "category.id = $id");
+        return $movies;
+    }
+
+
+}
+
