8ca3076f35aea60cdbc5893a16e18685f38890ee
Killian Herlant <killian.herlant@gmail.com>
2024-05-01 12:09:05 +0200

    added Controller.php and CinemaController.php

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,1 +1,68 @@
 <?php
+
+require_once '../Model/Repository/EntityRepository.php';
+
+class Controller {
+
+    protected EntityRepository $entityRepository;
+
+    public function __construct(EntityRepository $entityRepository)
+    {
+        $this->entityRepository = $entityRepository;
+    }
+
+    public function view($view, $data = []) {
+        require_once '../Template/' . $view . '.html.php';
+    }
+
+    /**
+     * @return array exemple : [0 => Cinema, 1 => Cinema, 2 => Cinema]
+     */
+
+    public function getAll() {
+        $data = $this->entityRepository->getAll();
+        $this->view('', $data);
+    }
+
+    /**
+     * @param int $id exemple : 1
+     * @return object exemple : Cinema {id: 1, name: "Cinema 1", address: "1 rue de Paris"}
+     */
+
+    public function getById(int $id) {
+        $data = $this->entityRepository->getById($id);
+        $this->view('', $data);
+    }
+
+    /**
+     * @param string $columns exemple : "name = 'Cinema 1', address = '1 rue de Paris'"
+     * @param string $filtre exemple : "id = 1"
+     * @return void
+     */
+
+    public function updateTable(string $columns, string $filtre) {
+        $this->entityRepository->updateTable($columns, $filtre);
+    }
+
+    /**
+     * @param string $filtre exemple : "id = 1"
+     * @return void
+     */
+
+    public function deleteFromTable(string $filtre) {
+        $this->entityRepository->deleteFromTable($filtre);
+    }
+
+    /**
+     * @param string $columns exemple : "name, address"
+     * @param string $values exemple : "'Cinema 1', '1 rue de Paris'"
+     * @return void
+     */
+
+    public function insertIntoTable(string $columns, string $values) {
+        $this->entityRepository->insertIntoTable($columns, $values);
+    }
+
+    
+
+}
