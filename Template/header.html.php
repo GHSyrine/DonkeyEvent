d445be8c5c785a26b166f73bd094a839d0c5d71b
Syrine GHRIBI <ghribi.syrine.ghs@gmail.com>
2024-05-01 15:40:22 +0200

    Added headerand edit home

---
+++ Template/header.html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Accueil</a>
+                </li>
+                <li>
+                    <a href="/">Cinémas</a>
+                </li>
+                <li>
+                    <a href="/">Films</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+</body>
+</html>
---
+++ Template/home.html.php
@@ -0,0 +1,7 @@
+<?php
+include '../Template/header.html.php';
+?>
+<h1>Welcome to DonkeyCinema </h1>
+<?php
+include '../Template/footer.html.php';
+?>
