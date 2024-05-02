fccad406a6f2b698bf72674880ae2ebd5b5086c5
Alexandra SALVADOR <apintosalvador19@gmail.com>
2024-05-02 18:39:47 +0200

    fix: ajout list movie all + ajout template movieone

--- Template/movieall.html.php
+++ Template/movieall.html.php
@@ -1,1 +1,8 @@
-<h1>Tout les films</h1>
+<h1>Tous les films</h1>
+<ul>
+<?php
+foreach($data as $movie) {
+    echo '<li><a href="/movie/one/'.$movie->getId().'">'.$movie->getName().'</a></li>';
+}
+?>
+</ul>
