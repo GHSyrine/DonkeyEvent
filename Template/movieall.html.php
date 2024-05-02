589c7c2739f2c0cdb2673636163b913b4b013ef6
Alexandra SALVADOR <apintosalvador19@gmail.com>
2024-05-02 19:33:35 +0200

    fix: update template

--- Template/movieall.html.php
+++ Template/movieall.html.php
@@ -2,13 +2,13 @@
 include 'Template/header.html.php';
 ?>
 <h1>Tous les films</h1>
-<ul>
-<?php
-foreach($data as $movie) {
-    echo '<li><a href="/movie/one/'.$movie->getId().'">'.$movie->getName().'</a></li>';
-}
-?>
-</ul>
+    <ul>
+    <?php foreach($data as $movie) : ?>
+        <li><a href="/movie/one/<?=$movie->getId()?>">
+            <?=$movie->getName()?></a>
+        </li>
+    <?php endforeach ?>
+    </ul>
 <?php
 include 'Template/footer.html.php';
 ?>
