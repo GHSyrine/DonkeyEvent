7d46550f6cf5eba07901d7f0eea74792584ff7fa
Alexandra SALVADOR <apintosalvador19@gmail.com>
2024-05-09 17:03:13 +0200

    ajout d'image et du front sur cinemaOne,movieOne et roomOne

--- Template/movieone.html.php
+++ Template/movieone.html.php
@@ -1,23 +1,36 @@
 <?php
 include 'Template/header.html.php';
 ?>
-<h1><?= $data->getName() ?></h1>
-<p><?= $data->getDescription() ?></p>
-<h2>Catégories du film : </h2>
-<?php
-if (!empty($data->getCategories())) :
-    foreach ($data->getCategories() as $category) : ?>
-        <a href="/category/one/<?= $category->getId() ?>" style="background:#d5acc7;"><b><?= $category->getName() ?></b></a>
-<?php endforeach;
-endif; ?>
+<body class="bg-dark">
+    <section class="container-movie ">
+        <h1 class="name text-white text-capitalize text-center"><?= $data->getName() ?></h1>
+        <div class="text-center ">
+            <img class="image-movie" src="<?= $data->getImage() ?>" alt="image du film">
+        </div>
 
-<h2>Séances du film : </h2>
-<?php
-if (!empty($data->getSeances())) :
-    foreach ($data->getSeances() as $seance) : ?>
-        <a href="/seance/one/<?= $seance->getId() ?>" style="background:#fcb2e8;padding:5px"><b><?= $seance->getPrice() ?>€</b> - <b><?= $seance->getDate() ?></b></a>
-<?php endforeach;
-endif; ?>
-<?php
-include 'Template/footer.html.php';
-?>
+        <p class="description text-white text-capitalize text-center
+        "><?= $data->getDescription() ?></p>
+        <h2 class="text-white text-center">Catégories du film : </h2>
+        <?php
+        if (!empty($data->getCategories())) :
+            foreach ($data->getCategories() as $category) : ?>
+                <a href="/category/one/<?= $category->getId() ?>" style="color:white"><b><?= $category->getName() ?></b></a>
+        <?php endforeach;
+        endif; ?>
+
+        <h2 class="text-white text-center ">Séances du film : </h2>
+        
+    <section class="cards seances">
+    <?php
+        if (!empty($data->getSeances())) :
+            foreach ($data->getSeances() as $seance) : ?>
+                <a href="/seance/one/<?= $seance->getId() ?>" style=" color:white;padding:5px"><b><?= $seance->getPrice() ?>€</b> - <b><?= $seance->getDate() ?></b></a>
+        <?php endforeach;
+        endif; ?>
+       
+    </section>
+    <?php
+        include 'Template/footer.html.php';
+        ?>
+    </section>
+</body>
--- Template/roomone.html.php
+++ Template/roomone.html.php
@@ -1,42 +1,37 @@
 <?php
 include 'Template/header.html.php';
 ?>
-<div class="container mt-5">
-    <h1>Nom de la salle : <?= $data->getTitle() ?></h1>
-    <p>Type : <?= $data->getType() ?></p>
-    <?php
-
+<body class="container-body bg-dark">
+    <div class="container mt-5">
+        <h1 class="salle text-center text-white text-capitalize"> <?= $data->getTitle() ?></h1>
+        <p class="type-seance text-capitalize text-white">Type : <?= $data->getType() ?></p>
+        <?php
     if (!empty($data->getSeances())) : ?>
-        <h2>Spectacles:</h2>
-        <nav class="mb-8">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <br>
-                    <?php $dates = []; ?>
-                </div>
+        <h2 class="title-seance text-capitalize text-white ">Séances:</h2>
+        <nav class=" card mb-8 bg-dark">
+            <div class="swiper-container text-white">
+            <?php $dates = [];?>
+
                 <?php foreach ($data->getSeances() as $seance) : ?>
                     <?php if (!in_array($seance->getDate(), $dates)) : ?>
                         <?php $dates[] = $seance->getDate(); ?>
                     <?= $seance->getDate();
                     endif; ?>
                     <br>
-                    <div>
-                        <?= $seance->getTime(); ?>
-                        <br>
-                        <?= $seance->getLangage(); ?>
-                    </div>
+                    <div><?= $seance->getTime(); ?><br><?= $seance->getLangage();?></a></div>
+            
+                    
                     <?php $movies = [];
                     foreach ($data->getMovies() as $movie) : ?>
                         <?php if ($movie->getId() == $seance->getMovie_id() && !in_array($movie->getId(), $movies)) : ?>
-                            <div>
                                 <?php $movies[] = $movie->getId(); ?>
-                                <a href="/movie/one/<?= $movie->getId() ?>"><?= $movie->getName() ?> </a>
+                                <a class="title-movie text-capitalize text-white" href="/movie/one/<?= $movie->getId() ?>"><?= $movie->getName()?></a>
+                                <p class="exit text-capitalize"> date de sortie : <?= $movie->getReleaseDate()?></p>
                                 <br>
-                        <?= "Release_date" . $movie->getReleaseDate();
-                        //ajouter l'image
-                        endif;
+                                
+                                <img src="<?=$movie->getImage()?>">
+                       <?php endif;
                     endforeach; ?>
-                            </div>
                         <?php
                     endforeach;
                     foreach ($dates as $date) : ?>
@@ -44,4 +39,9 @@ include 'Template/header.html.php';
                                 <?= $date; ?>
                             </div>
                     <?php endforeach;
-                endif; ?>
+                    
+                endif;
+            include 'Template/footer.html.php';
+            ?>
+    </div>
+</body>
