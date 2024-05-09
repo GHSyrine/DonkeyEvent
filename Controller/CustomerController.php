497ae57fc6b214792fa65dd78a22b608bae2da79
Syrine GHRIBI <ghribi.syrine.ghs@gmail.com>
2024-05-09 18:19:24 +0200

    update reservation

--- Controller/CustomerController.php
+++ Controller/CustomerController.php
@@ -15,7 +15,7 @@ Class CustomerController extends Controller{
 
     public function reserve() {
         var_dump($_POST);
-        die();
+       
     }
 
 }
--- Template/seancereserve.html.php
+++ Template/seancereserve.html.php
@@ -9,12 +9,14 @@ include 'Template/header.html.php';
 <h2>Dans la salle <b><?= $data->getRoom()->getTitle() ?></b></h2>
 <h2>Place(s) : <b><?= implode(", ", $data->getSeats()); ?></b></h2>
 
-<form action="/customer/reserve" method="POST">
+<form action="/customer/reserve/<?= $data->getId() ?>" method="POST">
     <input type="hidden" name="movieName" value="<?= $data->getMovies()[0]->getName() ?>">
     <input type="hidden" name="date" value="<?= $data->getDate() ?>">
     <input type="hidden" name="time" value="<?= $data->getTime() ?>">
     <input type="hidden" name="roomTitle" value="<?= $data->getRoom()->getTitle() ?>">
     <input type="hidden" name="seats" value="<?= implode(", ", $data->getSeats()); ?>">
+    <input type="hidden" name="seanceId" value="<?= $data->getId() ?>">
+    
     <input type="submit" value="Confirmer la réservation">
 </form>
 
